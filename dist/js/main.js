b03978004f2c4676bdf6406f9c49811c169215b7
Mikus Hartmanis <mikus.hartmanis@gmail.com>
2020-01-23 10:34:16 +0200

    Finished Menu Overlay

---
+++ dist/js/main.js
@@ -0,0 +1,40 @@
+// Select DOM items
+
+// DOM = Document object model = UI structure / model / attributes / tags etc...
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+// set initial State of Menu
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if(!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add ('show'));
+
+        // Set Menu State
+
+        showMenu = true;
+
+    } else {   // do the opposite of the If since we want the opposite to happen.
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        // Set Menu State
+
+        showMenu = false;
+    }
+}
